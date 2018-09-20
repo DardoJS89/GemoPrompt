3ba8de78398ec1884f1c1df53ba42c9c50ccb6f1
Dardo Sabarots <Dardojs89@live.com>
2018-09-20 12:08:48 -0400

    Added States to reports generated

--- report.rb
+++ report.rb
@@ -1,5 +1,5 @@
 require 'prawn'
- 
+require 'pry'
 class Report
   def initialize(country)
     @country = country
@@ -10,6 +10,15 @@ class Report
       pdf.text @country.name
       pdf.move_down 20
       pdf.text "Currency: #{@country.currency.name}"
-    end
+    if @country.states.any?
+        pdf.move_down 20
+        pdf.text "States:"
+        pdf.move_down 20
+
+        @country.states.each do |code, state|
+          pdf.text state["name"]
+        end
+      end
   end
+end
 end
