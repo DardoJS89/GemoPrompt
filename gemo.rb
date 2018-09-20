5ff688c39d2cf6ad777245bcd862d0c472a079ce
Dardo Sabarots <Dardojs89@live.com>
2018-09-20 11:50:43 -0400

    Added PDF Generator Support

--- gemo.rb
+++ gemo.rb
@@ -1,4 +1,5 @@
 require 'countries'
+require './report'
  
 puts "What country do you want a report on?"
  
@@ -7,7 +8,9 @@ country_name = gets.strip
 country = ISO3166::Country.find_country_by_name(country_name)
  
 if country
-  puts "The flag for #{country.name} is #{country.emoji_flag}"
+  puts "The flag of #{country.name} is #{country.emoji_flag}"
+  Report.new(country).run
+  puts "For a full report, open country_report.pdf"
 else
   puts "Could not find a country called #{country_name}"
 end
